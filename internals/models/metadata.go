16c64f1ea972a882a3e82703aa7ba4e31c073a41
saphalpdyl <saphalpdyl@gmail.com>
2024-12-02 19:59:00 -0600

    add: metadata model for metadata.json

---
+++ internals/models/metadata.go
@@ -0,0 +1,17 @@
+package models
+
+type FileMetadata struct {
+	FilePath string            `json:"filepath"`
+	Metadata map[string]string `json:"metadata"`
+}
+
+type GroupData struct {
+	Group string         `json:"group"`
+	Files []FileMetadata `json:"files"`
+}
+
+type RootMetaData struct {
+	LastUpdated int64 `json:"last_updated"`
+
+	Data []GroupData `json:"data"`
+}
