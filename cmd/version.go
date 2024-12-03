984e565104f3d5565745ac46f42f0e3ae37ab8f9
saphalpdyl <saphalpdyl@gmail.com>
2024-12-03 00:33:20 -0600

    add: gcms version command

---
+++ cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/saphalpdyl/gcms/helpers"
+	"github.com/spf13/cobra"
+)
+
+var versionCommand = &cobra.Command{
+	Use:   "version",
+	Short: "Gives the version of the installation",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(helpers.RenderBold("GCMS Version v1.1.0"))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCommand)
+}
