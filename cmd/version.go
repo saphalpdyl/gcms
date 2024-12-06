e70569ee3c19635ef210aa191c6388dbd721b589
saphalpdyl <saphalpdyl@gmail.com>
2024-12-06 01:43:29 -0600

    update: version to v1.2.1

--- cmd/version.go
+++ cmd/version.go
@@ -11,7 +11,7 @@ var versionCommand = &cobra.Command{
 	Use:   "version",
 	Short: "Gives the version of the installation",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(helpers.RenderBold("GCMS Version v1.2.0"))
+		fmt.Println(helpers.RenderBold("GCMS Version v1.2.1"))
 	},
 }
 
