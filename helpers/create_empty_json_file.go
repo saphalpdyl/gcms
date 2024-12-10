0e87ee3fbc404aceb1e778df3b918937d0eefada
saphalpdyl <saphalpdyl@gmail.com>
2024-12-09 18:04:48 -0600

    add: create empty json file helper function

---
+++ helpers/create_empty_json_file.go
@@ -0,0 +1,29 @@
+package helpers
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/saphalpdyl/gcms/internals/models"
+)
+
+func CreateEmptyJsonFile(folderPath string) error {
+	file, err := os.Create("schema.json")
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+
+	defer file.Close()
+
+	jsonData := make(models.SchemaMap)
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", " ")
+
+	if err = encoder.Encode(jsonData); err != nil {
+		return fmt.Errorf("Couldn't encode json")
+	}
+
+	return nil
+}
